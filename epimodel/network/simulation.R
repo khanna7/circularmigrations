1e6717d9fbadc4521794ce6454da408e4af95262
ngill25 <gilln@uchicago.edu>
2015-07-31 09:05:56 -0500

    fixed contacts per week

--- epimodel/network/simulation.R
+++ epimodel/network/simulation.R
@@ -14,7 +14,7 @@ target.stats <- c(3000, 0, rep(0, 2), rep(0, 3), 500, rep(0, 2), 0, 1000, 500, 0
 
 est_migrations <- netest(nw_migrations, formation = formation, target.stats = target.stats, coef.diss = dissolution_coefs(~offset(edges), 100))
 
-param <- param.net(death.rate.gen = 1/(45*52), death.rate.aids = 1/40, migration.rate = 1/30, birth.rate = 8*1250/(45*52), late.cutoff = 512, inf.prob = c(rep(1-(1-.0007*26)^3, 12), rep(1-(1-.0007)^3, 500), rep(1-(1-.0007*7)^3, 40)))
+param <- param.net(death.rate.gen = 1/(45*52), death.rate.aids = 1/40, migration.rate = 1/30, birth.rate = 8*1250/(45*52), late.cutoff = 512, inf.prob = c(rep(1-(1-.0007*26)^2.4, 12), rep(1-(1-.0007)^2.4, 500), rep(1-(1-.0007*7)^2.4, 40)))
 
 source('~/circularmigrations/epimodel/network/migration.R')
 source('~/circularmigrations/epimodel/network/infection.R')
