c4ac435268f414e954b7c090c8092cf31c16cbb1
Aditya Khanna <khanna7@uchicago.edu>
2015-10-01 15:07:07 -0500

    testing history and code

--- epimodel/network/Restricted_Model/restricted_model_simulation.R
+++ epimodel/network/Restricted_Model/restricted_model_simulation.R
@@ -3,17 +3,18 @@ rm(list=ls())
 #library(EpiModel) 
 load("estimation_restricted.RData")
 
-# source("migration_restricted.R")
-# source("infection_restricted.R")
-# source("births_restricted.R")
-# source("deaths_restricted.R")
-# source("get_prev_restricted_example_4.R")
-# source("initialize_net_restricted.R")
-# source("verbose_restricted_example.R")
+source("migration_restricted.R")
+source("infection_restricted.R")
+source("births_restricted.R")
+source("deaths_restricted.R")
+source("get_prev_restricted_example_4.R")
+source("initialize_net_restricted.R")
+source("verbose_restricted_example.R")
+
 nsim <- 1
 
 #control <- control.net(type="SI", nsims = 1, nsteps = 200, initialize.FUN = initialize.net.mig, deaths.FUN = deaths, births.FUN = births, edges_correct.FUN = edges_correct, infection.FUN = infection, migration.FUN = migration, get_prev.FUN = get_prev, verbose.FUN = verbose, depend = TRUE)
-control <- control.net(type = "SI", nsims = 1, nsteps = 5000, initialize.FUN = initialize.net.mig,
+control <- control.net(type = "SI", nsims = 1, nsteps = 3, initialize.FUN = initialize.net.mig,
                        deaths.FUN = deaths, births.FUN = births, infection.FUN = infection,
                        migration.FUN = migration,
                        get_prev.FUN = get_prev_example_4,
